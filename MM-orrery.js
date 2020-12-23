40db99198cc65b409634b67d2e2d77c0ea63f2cc
Dave Hoy <hoyski@gmail.com>
2020-12-23 07:02:50 -0500

    Added orbits General code cleanup and optimization

--- MM-orrery.js
+++ MM-orrery.js
@@ -2,205 +2,213 @@
  * Orrery (planet model) module for MagicMirror.
  */
 Module.register("MM-orrery", {
-    // Default module config
-    defaults: {
-        width: 900,
-        height: 800,
-        updateInterval: 24, // Default to daily updates
-        showLabels: true,
-        labelColor: 'gray',
-        labelFont: '12px Arial',
+	// Default module config
+	defaults: {
+		width: 900,
+		height: 800,
+		updateInterval: 24, // Default to daily updates
+		showLabels: true,
+		labelColor: 'gray',
+		labelFont: '10px Helvetica',
+		showOrbits: true,
+		orbitColor: '#282828',
 
-        // Scales
-        jupiterScale: 0.4,
-        saturnScale: 0.25,
-        uranusScale: 0.15,
-        neptuneScale: 0.15,
+		// Scales
+		jupiterScale: 0.4,
+		saturnScale: 0.25,
+		uranusScale: 0.15,
+		neptuneScale: 0.15,
 
-        // Drawing sizes
-        sunRadius: 8,
-        mercuryRadius: 3,
-        venusRadius: 5,
-        earthRadius: 5,
-        marsRadius: 4,
-        jupiterRadius: 8,
-        saturnRadius: 7,
-        uranusRadius: 6,
-        neptuneRadius: 6,
+		// Drawing sizes
+		sunRadius: 8,
+		mercuryRadius: 3,
+		venusRadius: 5,
+		earthRadius: 5,
+		marsRadius: 4,
+		jupiterRadius: 8,
+		saturnRadius: 7,
+		uranusRadius: 6,
+		neptuneRadius: 6,
 
-        // Sun and planet colors
-        sunColor: 'yellow',
-        /* All others default to white */
+		// Sun and planet colors
+		sunColor: 'yellow',
+		/* All others default to white */
 
-        debug: false
-    },
+		debug: false
+	},
 
-    planets: [
-        {
-            name: "Mercury",
-            xyz: undefined
-        },
-        {
-            name: "Venus",
-            xyz: undefined
-        },
-        {
-            name: "Earth",
-            xyz: undefined
-        },
-        {
-            name: "Mars",
-            xyz: undefined
-        },
-        {
-            name: "Jupiter",
-            xyz: undefined
-        },
-        {
-            name: "Saturn",
-            xyz: undefined
-        },
-        {
-            name: "Uranus",
-            xyz: undefined
-        },
-        {
-            name: "Neptune",
-            xyz: undefined
-        }
-    ],
+	planets: [
+		makePlanet("Mercury", 1, 88),
+		makePlanet("Venus", 2, 225 / 2 + 1),
+		makePlanet("Earth", 5, 365 / 5 + 1),
+		makePlanet("Mars", 10, 687 / 10 + 1),
+		makePlanet("Jupiter", 50, 4332 / 50 + 1),
+		makePlanet("Saturn", 50, 10761 / 50 + 1),
+		makePlanet("Uranus", 50, 30685 / 50 + 1),
+		makePlanet("Neptune", 50, 60191 / 50 + 1)
+	],
 
-    // Returns configuration value for the given planet and config type or 'dfltVal' if none has been defined
-    getPlanetConfig: function (planetName, configType, dfltVal) {
-        var value = this.config[planetName.toLowerCase() + configType];
-        if (value == undefined) {
-            return dfltVal;
-        }
-        return value;
-    },
+	// Returns configuration value for the given planet and config type or 'dfltVal' if none has been defined
+	getPlanetConfig: function (planetName, configType, dfltVal) {
+		var value = this.config[planetName.toLowerCase() + configType];
+		if (value == undefined) {
+			return dfltVal;
+		}
+		return value;
+	},
 
-    // Returns the scaling factor for the given planet or 'dfltVal' if none has been defined
-    getPlanetScale: function (planetName, dfltVal) {
-        return this.getPlanetConfig(planetName, 'Scale', dfltVal);
-    },
+	// Returns the scaling factor for the given planet or 'dfltVal' if none has been defined
+	getPlanetScale: function (planetName, dfltVal) {
+		return this.getPlanetConfig(planetName, 'Scale', dfltVal);
+	},
 
-    // Returns the radius in pixels to rander the given planet or 'dfltVal' if none has been defined
-    getPlanetRadius: function (planetName, dfltVal) {
-        return this.getPlanetConfig(planetName, 'Radius', dfltVal);
-    },
+	// Returns the radius in pixels to rander the given planet or 'dfltVal' if none has been defined
+	getPlanetRadius: function (planetName, dfltVal) {
+		return this.getPlanetConfig(planetName, 'Radius', dfltVal);
+	},
 
-    // Returns the color to render the given planet or 'dfltVal' if none has been defined
-    getPlanetColor: function (planetName, dfltVal) {
-        return this.getPlanetConfig(planetName, 'Color', dfltVal);
-    },
+	// Returns the color to render the given planet or 'dfltVal' if none has been defined
+	getPlanetColor: function (planetName, dfltVal) {
+		return this.getPlanetConfig(planetName, 'Color', dfltVal);
+	},
 
-    getScripts: function () {
-        return ['planetCalcs.js']
-    },
+	getScripts: function () {
+		return ['planetCalcs.js']
+	},
 
+	getDom: function () {
+		var date = new Date();
 
-    getDom: function () {
-        var date = new Date();
+		Log.log("Drawing planet positions for " + date);
 
-        Log.log("Drawing planet positions for " + date);
+		// Create the div for the module and the canvas for the drawing
+		var wrapper = document.createElement("div");
 
-        // Create the div for the module and the canvas for the drawing
-        var wrapper = document.createElement("div");
+		var canvas = document.createElement("canvas");
 
-        var canvas = document.createElement("canvas");
+		// Add something to make the HTML different from the previous rendering 
+		// so that MagicMirror will redraw the canvas
+		canvas.setAttribute("id", "MM_orrery_" + date.getTime());
+		wrapper.appendChild(canvas);
 
-        // Add something to make the HTML different from the previous rendering 
-        // so that MagicMirror will redraw the canvas
-        canvas.setAttribute("id", "MM_orrery_" + date.getTime()); 
-        wrapper.appendChild(canvas);
+		canvas.width = this.config.width;
+		canvas.height = this.config.height;
+		var halfCWidth = canvas.width / 2;
+		var halfCHeight = canvas.height / 2;
 
-        canvas.width = this.config.width;
-        canvas.height = this.config.height;
+		var ctx = canvas.getContext("2d");
+		canvas.style.background = document.body.style.background;
 
-        var ctx = canvas.getContext("2d");
+		ctx.font = this.config.labelFont;
 
-        ctx.font = this.config.labelFont;
+		// Calculate the XYZ positions for each planet and record the maximum values found
+		var maxX = 0;
+		var maxY = 0;
+		for (let planet of this.planets) {
+			var xyz = getXyzForPlanet(planet.name, date);
 
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+			xyz.scale(this.getPlanetScale(planet.name, 1.0));
 
-        // Calculate the XYZ positions for each planet
-        for (var i = 0; i < this.planets.length; ++i) {
-            var xyz = getXyzForPlanet(this.planets[i].name, date);
+			planet.xyz = xyz;
 
-            //xyz.scale(this.planets[i].scale);
-            xyz.scale(this.getPlanetScale(this.planets[i].name, 1.0));
+			if (Math.abs(planet.xyz.x) > maxX) {
+				maxX = Math.abs(planet.xyz.x);
+			}
+			if (Math.abs(planet.xyz.y) > maxY) {
+				maxY = Math.abs(planet.xyz.y);
+			}
+		}
 
-            this.planets[i].xyz = xyz;
-        }
+		// Calculate the scaling factor from AUs to canvas
+		var maxXY = Math.max(maxX, maxY);
+		var minWH = Math.min(canvas.width, canvas.height) / 2;
+		var solarScale = minWH / maxXY;
 
-        // Calculate the scaling factor to make all of the planets fit on the canvas
-        var maxX = 0;
-        var maxY = 0;
+		// Draw the orbits the cheap way by calculating the planets' positions
+		// through one orbit into the future drawing a line along the way
+		if (this.config.showOrbits) {
+			for (let planet of this.planets) {
+				let date = new Date();
 
-        for (var i = 0; i < this.planets.length; ++i) {
-            if (Math.abs(this.planets[i].xyz.x) > maxX) {
-                maxX = Math.abs(this.planets[i].xyz.x);
-            }
-            if (Math.abs(this.planets[i].xyz.y) > maxY) {
-                maxY = Math.abs(this.planets[i].xyz.y);
-            }
-        }
-        var maxXY = Math.max(maxX, maxY);
-        var minWH = Math.min(canvas.width, canvas.height) / 2;
-        var solarScale = minWH / maxXY;
+				ctx.strokeStyle = this.config.orbitColor;
+				ctx.beginPath();
+				for (let i = 0; i < planet.orbitNumSteps; ++i, date.setDate(date.getDate() + planet.orbitStepDays)) {
+					var xyz = getXyzForPlanet(planet.name, date);
+					xyz.scale(this.getPlanetScale(planet.name, 1.0));
 
+					var centerX = xyz.x * solarScale + canvas.width / 2;
+					var centerY = halfCHeight - xyz.y * solarScale;
+					if (i == 0) {
+						ctx.moveTo(centerX, centerY);
+					} else {
+						ctx.lineTo(centerX, centerY);
+					}
+				}
+				ctx.stroke();
+			}
+		}
 
-        // Draw the sun
-        ctx.beginPath();
-        ctx.fillStyle = this.getPlanetColor('sun');
-        var sunRadius = this.getPlanetRadius('sun');
-        ctx.arc(canvas.width / 2, canvas.height / 2, sunRadius, 0, 2 * Math.PI, false);
-        ctx.fill();
+		// Draw the sun
+		ctx.beginPath();
+		ctx.fillStyle = this.getPlanetColor('sun');
+		var sunRadius = this.getPlanetRadius('sun');
+		ctx.arc(halfCWidth, halfCHeight, sunRadius, 0, 2 * Math.PI, false);
+		ctx.fill();
 
-        // Draw the planets
-        for (var i = 0; i < this.planets.length; ++i) {
+		// Draw the planets
+		for (let planet of this.planets) {
 
-            // Convert heliocentric AU co-ordinates to canvas pixel co-ordinates
-            var centerX = this.planets[i].xyz.x * solarScale + canvas.width / 2;
-            var centerY = canvas.height / 2 - this.planets[i].xyz.y * solarScale;
+			// Convert heliocentric AU co-ordinates to canvas pixel co-ordinates
+			var centerX = planet.xyz.x * solarScale + halfCWidth;
+			var centerY = halfCHeight - planet.xyz.y * solarScale;
 
-            // Draw the planet
-            var radius = this.getPlanetRadius(this.planets[i].name, 5);
+			// Draw the planet
+			var radius = this.getPlanetRadius(planet.name, 5);
 
-            ctx.fillStyle = this.getPlanetColor(this.planets[i].name, 'white');
-            ctx.beginPath();
-            ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            ctx.fill();
+			ctx.fillStyle = this.getPlanetColor(planet.name, 'white');
+			ctx.beginPath();
+			ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+			ctx.fill();
 
-            // Draw the label
-            if (this.config.showLabels == true) {
-                ctx.beginPath();
-                ctx.fillStyle = this.config.labelColor;
-                ctx.fillText(this.planets[i].name, centerX + radius + 2, centerY - radius);
-                ctx.fill();
-            }
-        }
+			// Draw the label
+			if (this.config.showLabels == true) {
+				ctx.beginPath();
+				ctx.fillStyle = this.config.labelColor;
+				ctx.fillText(planet.name, centerX + radius + 2, centerY - radius);
+				ctx.fill();
+			}
+		}
 
-        if (this.config.debug) {
-            ctx.beginPath();
-            ctx.fillStyle = 'white';
-            ctx.fillText("Redrew planets at " + date, 10, canvas.height);
-            ctx.fill();
-        }
-        
-        return wrapper;
-    },
+		if (this.config.debug) {
+			ctx.beginPath();
+			ctx.fillStyle = 'white';
+			ctx.fillText("Redrew planets at " + date, 10, canvas.height);
+			ctx.fill();
+		}
 
-    start: function () {
-        var updateInterval = this.config.updateInterval * 60 * 60 * 1000;
+		return wrapper;
+	},
 
-        var self = this;
+	start: function () {
+		var updateInterval = this.config.updateInterval * 60 * 60 * 1000;
 
-        setInterval(function() {
-            self.updateDom();
-        }, updateInterval);
+		var self = this;
 
-        Log.log(this.name + " is started. Updates scheduled for every " + updateInterval + " ms");
-    }
+		setInterval(function () {
+			self.updateDom();
+		}, updateInterval);
+
+		Log.log(`${this.name} is started. Updates scheduled for every ${updateInterval} ms`);
+	}
 });
+
+
+// Returns an object with the name of the planet and the stepping values
+// for drawing the orbit
+function makePlanet(name, orbitStepDays, orbitNumSteps) {
+	return {
+		name,
+		orbitStepDays,
+		orbitNumSteps
+	};
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A module for MagicMirror<sup>2</sup> that displays all of the planets and their current relation to the Sun
 
 ## Dependencies
-  * A [MagicMirror<sup>2</sup>](https://github.com/MichMich/MagicMirror) installation
+  * A [MagicMirror<sup>2</sup>](https://magicmirror.builders/) installation
 
 ## Installation
   1. Clone this repo into your `modules` directory.
@@ -26,16 +26,18 @@ A module for MagicMirror<sup>2</sup> that displays all of the planets and their 
 }, // Note: Remove the comma if MM-orrery is the last module in your list
 ```
 ## Config
-| **Option** | **Default** | **Description** |
-| --- | --- | --- |
-| `width` | 900 | Width of the module in pixels. *Note:* Set the width greater than the height if Neptune's label is truncated  |
-|`height`| 800 | Height of the module in pixels. |
-|`updateInterval`| 24 | Time between updates in hours |
-|`showLabels`| true | Whether or not to display each planet's name |
-|`labelColor`| gray | Color for the labels |
-|`labelFont`| 12px Arial | Font for the labels |
-|`mercuryScale` thru `neptuneScale`| See description | The solar system is big, like *really* big. In order for the outer planets to fit on the screen their distances from the Sun have to be reduced. The eight *planet*Scale settings define multipliers applied to the planets' positions. The defaults are 1.0 for Mercury thru Mars, 0.4 for Jupiter, 0.25 for Saturn, and 0.15 for Uranus and Neptune. |
-|`sunRadius` and `mercuryRadius` thru `neptuneRadius`| See description | Render radius for the Sun and each planet in pixels. The defaults are 8 for the Sun, 3 for Mercury, 5 for Venus and Earth, 4 for Mars, 8 for Jupiter, 7 for Saturn, and 6 for Uranus and Neptune |
-|`sunColor` and `mercuryColor` thru `neptuneColor` | See description | Render color for the Sun and each planet. Defaults are 'yellow' for the Sun and 'white' for all of the planets |
+| **Option**                                           | **Default**     | **Description**                                                                                                                                                                                                                                                                                                                                        |
+| ---------------------------------------------------- | --------------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ |
+| `width`                                              | 900             | Width of the module in pixels. *Note:* Set the width greater than the height if Neptune's label is truncated                                                                                                                                                                                                                                           |
+| `height`                                             | 800             | Height of the module in pixels.                                                                                                                                                                                                                                                                                                                        |
+| `updateInterval`                                     | 24              | Time between updates in hours                                                                                                                                                                                                                                                                                                                          |
+| `showLabels`                                         | true            | Whether or not to display each planet's name                                                                                                                                                                                                                                                                                                           |
+| `labelColor`                                         | gray            | Color for the labels                                                                                                                                                                                                                                                                                                                                   |
+| `labelFont`                                          | 10px Helvetica  | Font for the labels                                                                                                                                                                                                                                                                                                                                    |
+| `showOrbits`                                         | true            | Whether or not to display the planets' orbits                                                                                                                                                                                                                                                                                                          |
+| `orbitColor`                                         | #282828         | Color for the orbits. Default is a very dark gray                                                                                                                                                                                                                                                                                                      |
+| `mercuryScale` thru `neptuneScale`                   | See description | The solar system is big, like *really* big. In order for the outer planets to fit on the screen their distances from the Sun have to be reduced. The eight *planet*Scale settings define multipliers applied to the planets' positions. The defaults are 1.0 for Mercury thru Mars, 0.4 for Jupiter, 0.25 for Saturn, and 0.15 for Uranus and Neptune. |
+| `sunRadius` and `mercuryRadius` thru `neptuneRadius` | See description | Render radius for the Sun and each planet in pixels. The defaults are 8 for the Sun, 3 for Mercury, 5 for Venus and Earth, 4 for Mars, 8 for Jupiter, 7 for Saturn, and 6 for Uranus and Neptune                                                                                                                                                       |
+| `sunColor` and `mercuryColor` thru `neptuneColor`    | See description | Render color for the Sun and each planet. Defaults are 'yellow' for the Sun and 'white' for all of the planets                                                                                                                                                                                                                                         |
 
 
